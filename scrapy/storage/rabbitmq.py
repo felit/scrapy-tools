02c3bba18f65d0228344d71cdc3292d1828bae2f
felit <nofelit@126.com>
2017-03-01 20:24:50 +0800

    初始化

---
+++ scrapy/storage/rabbitmq.py
@@ -0,0 +1,8 @@
+class RabbitMQSignal():
+    def __init__(self):
+        print 'test rabbitmq signal'
+        pass
+
+    @classmethod
+    def cd(cls):
+        pass
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding:utf8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="Scrapy tools",
+    version="0.0.1",
+    description="tools for scrapy, consist of middlewares",
+    author="jack.cong",
+    url="http://www.livedrof.com",
+    license="LGPL",
+    packages=find_packages()
+    # ,
+    # scripts=["mytest/test.py"]
+)
