91e221a8c50c5ef74b8f485d2db7dda98abac305
felit <nofelit@126.com>
2017-03-02 00:35:23 +0800

    测试通过

--- test/tools/spiders/dianping.py
+++ test/tools/spiders/dianping.py
@@ -10,11 +10,18 @@ class DianpingSpider(scrapy.Spider):
     custom_settings = {
         'EXTENSIONS': {
             'scrapy_tools.storage.rabbitmq.RabbitMQSignal': 200
-        },
+        },'DEFAULT_REQUEST_HEADERS': {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Host': 'www.dianping.com',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
+        }
     }
 
     def parse(self, response):
         item = DianpingItem()
         item['title'] = 'dian ping'
-
-        pass
+        yield item
